5d92c6f9896312dd497324ddde99d816015b8e21
Pat desktop <dmtpatricia@gmail.com>
2026-06-12 16:40:00 +0200

    Discount controller + minor

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/DiscountController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/discounts")
+public class DiscountController {
+
+}
